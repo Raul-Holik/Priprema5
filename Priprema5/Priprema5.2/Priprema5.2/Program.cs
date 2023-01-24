e8df869f0bc29cc2f8fc8c2671068d5db5ad5581
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-24 19:44:02 +0100

    Add files via upload

---
+++ Priprema5/Priprema5.2/Priprema5.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema5._2
+{
+    internal class Program
+    {
+        class GeometrijskiLik
+        {
+            protected byte BrojStranica;
+            protected double[] Stranice = new double[10];
+            public void UcitajStranice()
+            {
+                for (int i = 0; i < BrojStranica; i++)
+                {
+                    Console.WriteLine("Upišite duljinu {0}. stranice", i + 1);
+                    string s = Console.ReadLine();
+                    Stranice[i] = Convert.ToDouble(s);
+                }
+            }
+            public double Opseg()
+            {
+                double opseg = 0;
+                for (int i = 0; i < BrojStranica; i++)
+                    opseg += Stranice[i];
+                return opseg;
+            }
+        }
+        class Trokut : GeometrijskiLik
+        {
+            public Trokut()
+            {
+                BrojStranica = 3;
+            }
+        }
+        class Cetverokut : GeometrijskiLik
+        {
+            public Cetverokut()
+            {
+                BrojStranica = 4;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Trokut t = new Trokut();
+            t.UcitajStranice();
+            Console.WriteLine("Opseg trokuta je: "+t.Opseg());
+            Cetverokut c = new Cetverokut();
+            c.UcitajStranice();
+            Console.WriteLine("Opseg četverokuta je: "+c.Opseg());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema5/Priprema5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema5
+{
+    internal class Program
+    {
+        public class Fruit
+        {
+            private string Vrsta;
+            private string Boja;
+            private bool JeLiSlatko;
+            public Fruit(string s)
+            {
+                Vrsta = s;
+            }
+            public Fruit(string s1, string s2, bool b)
+            {
+                Vrsta = s1;
+                Boja = s2;
+                JeLiSlatko = b;
+            }
+            static void Main(string[] args)
+            {
+                Fruit x = new Fruit("Jagoda", "Crvena", true);
+                Fruit y = new Fruit("Limun", "Žuta", false);
+                Fruit z = new Fruit("Avokado");
+
+                
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
